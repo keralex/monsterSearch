2a06d4d7cff0c8792409cfc3a81de8737d91dadf
keralex <kerlis.aguado26@gmail.com>
2019-10-29 13:22:46 -0300

    Updates

---
+++ precache-manifest.a4c0e92a1b471dc95ecca9f0531da064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68831fb7a35e243ac637b64c9ce770a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d14e4abd02c1286a299a",
+    "url": "/pokedex/static/css/main.4f4680a7.chunk.css"
+  },
+  {
+    "revision": "558b75da1465e2c7f4b3",
+    "url": "/pokedex/static/js/2.0cfa908f.chunk.js"
+  },
+  {
+    "revision": "d14e4abd02c1286a299a",
+    "url": "/pokedex/static/js/main.c2339df2.chunk.js"
+  },
+  {
+    "revision": "27f16e0463aa7cf68513",
+    "url": "/pokedex/static/js/runtime-main.dc93633d.js"
+  }
+]);
